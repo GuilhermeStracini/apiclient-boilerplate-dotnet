869b4ec41e00a21a9c81caac4babd3a803f25866
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-05-17 22:06:14 +0100

    Add more commands

--- intial-setup.ps1
+++ intial-setup.ps1
@@ -5,12 +5,20 @@ $GitHubRepo = Read-Host -Prompt 'GitHub repository'
 $AppVeyorId = Read-Host -Prompt 'AppVeyor project ID'
 $CompanyName = Read-Host -Prompt 'Company/Author name (package copyright)'
 
-(Get-Content README.md)  ForEach-Object { $_ -replace "{username}", $GitHubUsername }  Set-Content README.md
-(Get-Content README.md)  ForEach-Object { $_ -replace "{repo}", $GitHubUserRepo }  Set-Content README.md
-(Get-Content README.md)  ForEach-Object { $_ -replace "{appVeyorId}", $AppVeyorId }  Set-Content README.md
+Get-Content README.md | Select-Object -Skip 19 | Out-File README.md
+(Get-Content README.md) | ForEach-Object { $_ -replace "{username}", $GitHubUsername }  Set-Content README.md
+(Get-Content README.md) | ForEach-Object { $_ -replace "{repo}", $GitHubUserRepo }  Set-Content README.md
+(Get-Content README.md) | ForEach-Object { $_ -replace "{appVeyorId}", $AppVeyorId }  Set-Content README.md
 (Get-Content README.md) | ForEach-Object { $_ -replace "SolutionName", $SolutionName } | Set-Content README.md
+(Get-Content README.md) | ForEach-Object { $_ -replace "API Client Boilerplate", $ProjectName } | Set-Content README.md
 (Get-Content README.md) | ForEach-Object { $_ -replace "GuilhermeStracini/apiclient-boilerplate-dotnet", "$GitHubUsername\$GitHubRepo" } | Set-Content README.md
 (Get-ChildItem -Recurse -Include *.cs | Get-Content) | ForEach-Object { $_ -replace "SolutionName", $SolutionName } | Set-Content $_.Fullname
 (Get-ChildItem -Recurse -Include *.csproj | Get-Content) | ForEach-Object { $_ -replace "SolutionName", $SolutionName } | Set-Content $_.Fullname.ToString().Replace('SolutionName', $SolutionName)
 (Get-Content SolutionName.sln) | ForEach-Object { $_ -replace "SolutionName", $SolutionName } | Set-Content ".\$SolutionName.sln"
-Remove-Item SOlutionName.sln
+Remove-Item SolutionName.sln
+Rename-Item -Path "Src/SolutionName/SolutionName.csproj" -NewName "Src/SolutionName/$SolutionName.csproj"
+Rename-Item -Path "Src/SolutionName" "Src/$SolutionName"
+Rename-Item -Path "Tests/SolutionName.IntegrationTests/SolutionName.IntegrationTests.csproj" -NewName "Tests/SolutionName.IntegrationTests/$SolutionName.IntegrationTest.csproj"
+Rename-Item -Path "Tests/SolutionName.IntegrationTests" "Tests/$SolutionName.IntegrationTests"
+Rename-Item -Path "Tests/SolutionName.UnitTests/SolutionName.UnitTests.csproj" -NewName "Tests/SolutionName.UnitTests/$SolutionName.UnitTests.csproj"
+Rename-Item -Path "Tests/SolutionName.UnitTests" "Tests/$SolutionName.UnitTests"
